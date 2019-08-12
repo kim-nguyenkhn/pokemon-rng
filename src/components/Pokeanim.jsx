46550ecfe3cc76f5486bfdc7c4349243c2e406ec
Gerard Salisi <gerardsalisi@lyft.com>
2019-08-12 16:38:10 -0700

    added opening animation

--- src/components/Pokeanim.jsx
+++ src/components/Pokeanim.jsx
@@ -1,15 +1,21 @@
 import React, { useEffect } from "react";
 import "./styles.css";
 
+const unopenedPokeball = "https://i.imgur.com/5oiq2uX.png";
+const openedPokeball = "https://i.imgur.com/ilZCT7n.png";
+
 export function PokeAnim() {
-  const handleClick = () => {
-    console.log('clicked');
+  const handleClick = (e) => {
+    const img = e.target;
+    img.src = openedPokeball;
+    img.classList = "";
   }
 
   return (
     <div style={{ backgroundColor: "black" }}>
       <img
-        src={"https://i.imgur.com/5oiq2uX.png"}
+      id="pokeball"
+        src={unopenedPokeball}
         className="pokeshake"
         onClick={handleClick}
       />
