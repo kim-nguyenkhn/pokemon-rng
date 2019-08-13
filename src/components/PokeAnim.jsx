a9d1d627d15facbb6bb4a3242fefd6bd8ab97f84
g2salisi <gerardsalisi@lyft.com>
2019-08-13 13:59:52 -0700

    added flash and pulse to animations

--- src/components/PokeAnim.jsx
+++ src/components/PokeAnim.jsx
@@ -8,27 +8,30 @@ export function PokeAnim() {
     const handleClick = e => {
         const img = e.target;
         const flashOverlay = document.getElementById('flash');
+        const pulseComponent = document.getElementById('pulse');
 
         img.classList.remove('poketwitch');
         img.classList.add('pokeshake');
+        pulseComponent.style.animation = 'pulse 0.8s infinite, shake 0.8s infinite';
         setTimeout(() => {
             img.classList = '';
             img.src = openedPokeball;
             flashOverlay.classList.add('flash');
+            pulseComponent.style.display = 'none';
         }, 2400);
     };
 
     return (
         <React.Fragment>
             <div className="overlay container">
-                <div style={{position: 'relative'}}>
+                <div style={{ position: 'relative' }}>
                     <img
                         id="pokeball"
                         src={unopenedPokeball}
                         className="poketwitch"
                         onClick={handleClick}
                     />
-                    <div class="pulse-div pulse poketwitch" />
+                    <div class="pulse-div pulse" id="pulse" />
                 </div>
             </div>
             <div className="overlay" id="flash" />
