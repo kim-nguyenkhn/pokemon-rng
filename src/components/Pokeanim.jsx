866deb649433fb2235f898990e03160c093a326d
Gerard Salisi <gerardsalisi@lyft.com>
2019-08-13 10:55:46 -0700

    wip

--- src/components/Pokeanim.jsx
+++ src/components/Pokeanim.jsx
@@ -1,24 +1,37 @@
-import React, { useEffect } from "react";
-import "./styles.css";
+import React, { useEffect } from 'react';
+import './styles.css';
 
-const unopenedPokeball = "https://i.imgur.com/5oiq2uX.png";
-const openedPokeball = "https://i.imgur.com/ilZCT7n.png";
+const unopenedPokeball = 'https://i.imgur.com/5oiq2uX.png';
+const openedPokeball = 'https://i.imgur.com/ilZCT7n.png';
 
 export function PokeAnim() {
-  const handleClick = (e) => {
-    const img = e.target;
-    img.src = openedPokeball;
-    img.classList = "";
-  }
+    const handleClick = e => {
+        const img = e.target;
+        const flashOverlay = document.getElementById('flash');
 
-  return (
-    <div style={{ backgroundColor: "black" }}>
-      <img
-      id="pokeball"
-        src={unopenedPokeball}
-        className="pokeshake"
-        onClick={handleClick}
-      />
-    </div>
-  );
+        img.classList.remove('poketwitch');
+        img.classList.add('pokeshake');
+        setTimeout(() => {
+            img.classList = '';
+            img.src = openedPokeball;
+            flashOverlay.classList.add('flash');
+        }, 2400);
+    };
+
+    return (
+        <React.Fragment>
+            <div className="overlay container">
+                <div style={{position: 'relative'}}>
+                    <img
+                        id="pokeball"
+                        src={unopenedPokeball}
+                        className="poketwitch"
+                        onClick={handleClick}
+                    />
+                    <div class="pulse-div pulse poketwitch" />
+                </div>
+            </div>
+            <div className="overlay" id="flash" />
+        </React.Fragment>
+    );
 }
